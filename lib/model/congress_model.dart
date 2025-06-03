5d868043972240cfe31cf9d92186284c2d09ee80
doumil <93338229+doumil@users.noreply.github.com>
2025-06-03 19:14:42 +0100

    congress change design

--- lib/model/congress_model.dart
+++ lib/model/congress_model.dart
@@ -1,23 +1,32 @@
+class Speaker {
+  final String name;
+  final String imageUrl;
+
+  Speaker({required this.name, required this.imageUrl});
+}
+
 class CongressClass {
-  int id;
-  String title;
+  final int id;
+  final String title; // e.g., "Opening | Welcome Address"
+  final String? subtitle; // If there's a secondary title
+  final bool isSessionOver; // To display "Session is over"
+  final String? date; // e.g., "mer., 16 avr. 2025"
+  final String? time; // e.g., "10:15 - 10:20 | Africa(Casablanca time)"
+  final String? location; // e.g., "GITEX Africa/Ai Stage"
+  final String? stage; // e.g., "Ai Stage"
+  final List<String>? tags; // e.g., ["GITEX Africa"]
+  final List<Speaker>? speakers; // List of speakers for this session
 
-
-
-
-  CongressClass(this.id, this.title);
-  factory CongressClass.fromJson(dynamic json) {
-    return CongressClass(json['id'] as int, json['title'] as String);
-  }
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'title': title,
-
-    };
-  }
-  @override
-  String toString() {
-    return 'firstname : $id,lastname : $title';
-  }
+  CongressClass({
+    required this.id,
+    required this.title,
+    this.subtitle,
+    this.isSessionOver = false,
+    this.date,
+    this.time,
+    this.location,
+    this.stage,
+    this.tags,
+    this.speakers,
+  });
 }
